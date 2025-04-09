39a99a173d71a8435cb36ace7450ab1f53c40445
jiojiostudio <jiojio@jesus.edu.pl>
2025-04-09 09:00:19 +0100

    Add files via upload
    JioJio AutoCo 1.0

---
+++ config.js
@@ -0,0 +1,6 @@
+export const config = {
+    apiEndpoint: "http://1.1.1.1:8000/api/verify",
+    telegramBotToken: "Your Bot Token",
+    telegramChatId: "Your Chat ID",
+    telegramGroupId: "Your Group ID"
+}
